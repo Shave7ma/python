2d1ce2a379ab833761e055eb6562bb12bf907684
Shave7ma <113388644+Shave7ma@users.noreply.github.com>
2024-02-12 15:33:42 +0300

    Update задание 6-8.py
    Добавлена третья функция: поиск в строке наибольшего количества подряд идущих цифр

--- лабораторная работа 1/задание 6-8.py
+++ лабораторная работа 1/задание 6-8.py
@@ -13,3 +13,12 @@ elif max(numbers).is_integer():
     print(u'Максимальное число: ', int(max(numbers)))
 else:
     print(u'Максимальное число: ', max(numbers))
+
+#Найти в строке наибольшее количество идущих подряд цифр
+
+numbers3 = re.findall(r'\d+', x)
+maxlen = 0
+for i in numbers3:
+    if len(i) > maxlen:
+        maxlen = len(i)
+print(u'Наибольшее количество идущих подряд цифр: ', maxlen)
