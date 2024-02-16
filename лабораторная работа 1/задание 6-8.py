cb94a0ca53b224d470fea8c2c82bcb78b0e974f2
Shave7ma <113388644+Shave7ma@users.noreply.github.com>
2024-02-16 10:59:42 +0300

    Update задание 6-8.py
    Добавлена возможность выбора задачи и убраны ненужные строчки кода

--- лабораторная работа 1/задание 6-8.py
+++ лабораторная работа 1/задание 6-8.py
@@ -3,33 +3,35 @@ import re
 print(u'Введите строку: ')
 x = input()
 
+print(u'Какую задачу хотите решать? Введите число от 1 до 3: ')
+n = int(input())
+
 #Найти в строке максимальное из имеющихся в ней вещественных чисел
 
-pattern = r'[-]?\d+(?:\.\d*)?'
-numbers = [float(item) for item in re.findall(pattern, x)]
-if len(numbers) == 0:
-    print(u'В строке нет вещественных чисел!')
-elif max(numbers).is_integer():
-    print(u'Максимальное число: ', int(max(numbers)))
-else:
-    print(u'Максимальное число: ', max(numbers))
+if n == 1:
+    pattern = r'[-]?\d+(?:\.\d*)?'
+    numbers = [float(item) for item in re.findall(pattern, x)]
+    if len(numbers) == 0:
+        print(u'В строке нет вещественных чисел!')
+    else:
+        print(u'Максимальное число: ', max(numbers))
 
 #Найти в строке минимальное из имеющихся в ней рациональных чисел
-    
-pattern = r'[-]?\d+(?:\.\d*)?'
-numbers2 = [float(item) for item in re.findall(pattern, x)]
-if len(numbers2) == 0:
-    print(u'В строке нет рациональных чисел!')
-elif min(numbers2).is_integer():
-    print(u'Минимальное число: ', int(min(numbers2)))
-else:
-    print(u'Минимальное число: ', min(numbers2))
+
+if n == 2:
+    pattern = r'[-]?\d+(?:\.\d*)?'
+    numbers2 = [float(item) for item in re.findall(pattern, x)]
+    if len(numbers2) == 0:
+        print(u'В строке нет рациональных чисел!')
+    else:
+        print(u'Минимальное число: ', min(numbers2))
 
 #Найти в строке наибольшее количество идущих подряд цифр
 
-numbers3 = re.findall(r'\d+', x)
-maxlen = 0
-for i in numbers3:
-    if len(i) > maxlen:
-        maxlen = len(i)
-print(u'Наибольшее количество идущих подряд цифр: ', maxlen)
+if n == 3:
+    numbers3 = re.findall(r'\d+', x)
+    maxlen = 0
+    for i in numbers3:
+        if len(i) > maxlen:
+            maxlen = len(i)
+    print(u'Наибольшее количество идущих подряд цифр: ', maxlen)
