4528acd2e8dd1d7d5bc251ec399bf16bb5bff42e
Shave7ma <113388644+Shave7ma@users.noreply.github.com>
2024-02-16 10:12:04 +0300

    Update задание 6-8.py
    Добавлена вторая функция: поиск наименьшего рационального числа в строке

--- лабораторная работа 1/задание 6-8.py
+++ лабораторная работа 1/задание 6-8.py
@@ -14,6 +14,17 @@ elif max(numbers).is_integer():
 else:
     print(u'Максимальное число: ', max(numbers))
 
+#Найти в строке минимальное из имеющихся в ней рациональных чисел
+    
+pattern = r'[-]?\d+(?:\.\d*)?'
+numbers2 = [float(item) for item in re.findall(pattern, x)]
+if len(numbers2) == 0:
+    print(u'В строке нет рациональных чисел!')
+elif min(numbers2).is_integer():
+    print(u'Минимальное число: ', int(min(numbers2)))
+else:
+    print(u'Минимальное число: ', min(numbers2))
+
 #Найти в строке наибольшее количество идущих подряд цифр
 
 numbers3 = re.findall(r'\d+', x)
